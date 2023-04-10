9b913c4ce464e189a2b2375f04d0212cb720d8ee
João Borrega <joaoborrega18@gmail.com>
2023-04-10 15:54:11 +0100

    Add files via upload

---
+++ day 28/day-28-start.py
@@ -0,0 +1,12 @@
+# Canvas Widget
+
+# Unlimited Positional Arguments
+# def add(*args):
+#     for n in args:
+#         print(n)
+#
+# # Unlimited Keyword Arguments
+# def add(**kw):
+#     x = kw.get("n1")
+#     y = kw.get("n2")
+#     print(x + y)
